fdbc2694d72383a70d1b945046f4157dbd0faedf
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-10 12:53:24 -0500

    Modificando el ApiResponser para el API Gateway y creando el metodo errorMessage

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -12,7 +12,7 @@ trait ApiResponser {
      * @return Illuminate/Http/JsonResponse
      */
     public function successResponse($data, $code = Response::HTTP_OK) {
-        return response()->json(['data' => $data], $code);
+        return response($data, $code)->header('Content-Type', 'application/json');
     }
 
     /**
@@ -23,5 +23,15 @@ trait ApiResponser {
      */
     public function errorResponse($message, $code) {
         return response()->json(['error' => $message, 'code' => $code], $code);
+    }
+
+    /**
+     * Build success response
+     * @param string|array $message
+     * @param int $code
+     * @return Illuminate/Http/JsonResponse
+     */
+    public function errorMessage($message, $code) {
+        return response($message, $code)->header('Content-Type', 'application/json');
     }
 }
