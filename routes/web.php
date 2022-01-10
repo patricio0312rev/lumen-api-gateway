5eb0ac5816975c3a71542dbaf9db428c1a1f8916
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-10 12:54:37 -0500

    Creando rutas para Book y Author

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'authors'], function() use ($router){
+    $router->get('/', 'AuthorController@index');
+    $router->post('/', 'AuthorController@store');
+    $router->get('/{author}', 'AuthorController@show');
+    $router->put('/{author}', 'AuthorController@update');
+    $router->patch('/{author}', 'AuthorController@update');
+    $router->delete('/{author}', 'AuthorController@destroy');
+});
+
+$router->group(['prefix' => 'books'], function() use ($router){
+    $router->get('/', 'BookController@index');
+    $router->post('/', 'BookController@store');
+    $router->get('/{book}', 'BookController@show');
+    $router->put('/{book}', 'BookController@update');
+    $router->patch('/{book}', 'BookController@update');
+    $router->delete('/{book}', 'BookController@destroy');
+});
