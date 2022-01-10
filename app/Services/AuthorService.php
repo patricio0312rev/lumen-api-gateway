f523e40c274df660818e1af968c27f9043c5fffa
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-10 14:18:09 -0500

    Configurando secret para cada microservicio

--- app/Services/AuthorService.php
+++ app/Services/AuthorService.php
@@ -13,9 +13,16 @@ class AuthorService {
      */
     public $baseUri;
 
+    /**
+     * The secret to consume the books service
+     * @var string
+     */
+    public $secret;
+
     public function __construct()
     {
         $this->baseUri = config('services.authors.base_uri');
+        $this->secret = config('services.authors.secret');
     }
 
     /**
